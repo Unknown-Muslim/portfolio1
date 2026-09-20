29592602891b533aebe8d4478fa78bc487f573e0
Unknown-Muslim <a.mohammedsidat@gmail.com>
2026-09-20 20:48:03 +0100

    .

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"n8HhsUVrFAHkm5NL3Ar7L5CQftyzpTnqRp96ljnhzlE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
